00a212f0c201ef7d80e055e75568e1d463be397a
B FAROOQ <bfarooq2022@gmail.com>
2025-01-22 17:48:46 +0530

    files added

--- be/index.js
+++ be/index.js
@@ -12,17 +12,18 @@ const app = express();
 
 app.use(express.json());
 
-// const corsOptions = {
-//     origin: process.env.NODE_ENV === "production"
-//       ? "https://gen-image-fe.vercel.app"
-//       : "http://localhost:4000",
-//     methods: "GET, POST, PUT, DELETE",
-//     allowedHeaders: "Content-Type, Authorization",
-//   };
+const corsOptions = {
+    origin: "https://gen-image-fe.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,  // Allow credentials
+    optionsSuccessStatus: 200, // For legacy browser support
+    exposedHeaders: ["Content-Length", "X-Total-Count"], // If you need to expose specific headers
+    maxAge: 86400 // Cache preflight request results for 24 hours
+};
   
   
-app.use(cors({ origin: '*' }));
-
+  app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send("Backend working");
